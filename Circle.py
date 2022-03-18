b880a8275cc8fe75e27a727b45047fa9dc4d84b7
MagnetoCraft <93604302+MagnetoCraft@users.noreply.github.com>
2022-03-18 13:18:15 +0100

    Add files via upload

--- Circle.py
+++ Circle.py
@@ -2,36 +2,38 @@ import pygame
 import os
 
 class Circle():
-    def __init__(self, x, y, size, vector_x, vector_y):
+    def __init__(self, x, y, size, vector_x, vector_y, time):
         self.x = x
         self.y = y
         self.vector_x = vector_x
         self.vector_y = vector_y
         self.size = size
+        self.time = time
 
     def update(self, x, y, bots):
-        x += - self.x + self.vector_x
-        y += - self.y + self.vector_y
-        
+        self.time -= 1
+        x -= self.x
+        y -= self.y
+
         if abs(self.vector_x) != 0:
-            self.vector_x = self.vector_x*63/64
-            if abs(self.vector_x) < 1:
+            self.vector_x = self.vector_x*127/128
+            if abs(self.vector_x) < 0.1:
                 self.vector_x = 0
         if abs(self.vector_y) != 0:
-            self.vector_y = self.vector_y*63/64
-            if abs(self.vector_y) < 1:
+            self.vector_y = self.vector_y*127/128
+            if abs(self.vector_y) < 0.1:
                 self.vector_y = 0
-        
+
         if abs(x) > 1 or abs(y) > 1:
-            l = (x**2+y**2)**0.37
+            l = (x**2+y**2)**0.5
             x = x/l
             y = y/l
-            self.x += x
-            self.y += y
+            self.x += x + self.vector_x
+            self.y += y + self.vector_y
         for bot in bots:
             x = bot.x - self.x
             y = bot.y - self.y
-            l = (x**2+y**2)**0.37
+            l = (x**2+y**2)**0.5
             if l < self.size/2:
                 self.size += bot.size
                 bots.remove(bot)
--- Player.py
+++ Player.py
@@ -8,7 +8,7 @@ class Player():
         self.screen = screen
         self.image = pygame.image.load(os.path.join("images","pizza.png")).convert_alpha()
         self.circles = []
-        self.circles.append(Circle(0, 0, 100, 0, 0))
+        self.circles.append(Circle(0, 0, 100, 0, 0, 0))
         self.x = 0
         self.y = 0
 
@@ -24,6 +24,31 @@ class Player():
             circle.update(x, y, bots)
             self.x += circle.x
             self.y += circle.y
+
+        new_circles = []
+        for circle in self.circles:
+            for circle2 in self.circles:
+                if circle != circle2:
+                    x = circle.x - circle2.x
+                    y = circle.y - circle2.y
+                    l = (x**2+y**2)**0.5
+                    if l <circle.size/2 + circle2.size/2:
+                        if circle.time < 1 and circle2.time < 1:
+                            x = 0.5
+                            y = 0.5
+                        if l > 1:
+                            circle.x += (x*(circle.size/2 + circle2.size/2 - l)/l)/2
+                            circle.y += (y*(circle.size/2 + circle2.size/2 - l)/l)/2
+                            circle2.x -= (x*(circle.size/2 + circle2.size/2 - l)/l)/2
+                            circle2.y -= (y*(circle.size/2 + circle2.size/2 - l)/l)/2
+                        if circle.time < 1 and circle2.time < 1 and (l <circle.size/2 or l <circle2.size/2):
+                            new_circles.append(Circle((circle.x+circle2.x)/2, (circle.y+circle2.y)/2, circle.size + circle2.size, 0, 0, 0))
+                            self.circles.remove(circle)
+                            self.circles.remove(circle2)
+        self.circles += new_circles
+
+
+
         self.x = self.x / len(self.circles)
         self.y = self.y / len(self.circles)
 
@@ -33,30 +58,30 @@ class Player():
             self.screen.blit(image, (circle.x - circle.size/2, circle.y - circle.size/2))
 
 
-    def split():
+    def split(self, x_screen, y_screen):
         # j'ai mis dans la boucle dans le main:
         #si on appuye sur espace, le player se split comme dans le je
         # il faut que tu fasses la methode qui fait ça ducoop
 
         # j'ai mis sur github le lien vers un mec qui a fait un agario on peut s'en inspirer
-        old_circle = self.circle
-        self.circle = []
-        for circle in old_circle:
+        old_circles = self.circles
+        self.circles = []
+        for circle in old_circles:
             if circle.size > 64:
                 x = circle.x
                 y = circle.y
                 size = circle.size
-                self.circle.append(Circle(x, y, circle.size/2, 0, 0))
-                
-                x, y = pygame.mouse.get_pos()
-                x += x_screen
-                y += y_screen
-                
-                x += - self.x + self.vector_x
-                y += - self.y + self.vector_y
-                
-                l = (x**2+y**2)**0.37
-                x = x/l
-                y = y/l
-                self.x += x
-                self.y += y
+                self.circles.append(Circle(x, y, circle.size//2, 0, 0, 600))
+
+                x_mouse, y_mouse = pygame.mouse.get_pos()
+                x_mouse += x_screen - x
+                y_mouse += y_screen - y
+
+                l = (x_mouse**2+y_mouse**2)**0.5
+                x_mouse = x_mouse/l
+                y_mouse = y_mouse/l
+
+                self.circles.append(Circle(x, y, circle.size//2, x_mouse*2, y_mouse*2, 600))
+            else:
+                self.circles.append(circle)
+
