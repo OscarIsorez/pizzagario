d6f1f2fec9443ddaa1ac7b59cf381ca17e0dc28a
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2022-03-23 20:21:16 +0100

    ecran de départ

--- main_tmp.py
+++ main_tmp.py
@@ -5,6 +5,8 @@ from Camera import Camera
 from Grid import Grid
 import time
 import sys
+import math
+import os
 # from Grid import Grid
 taille_fenetre = (640, 480)
 
@@ -18,7 +20,6 @@ def fonctions_player():
     player.update(bots)
 
 
-
 if __name__ == "__main__":
     pygame.init()
     pygame.font.init()
@@ -26,6 +27,17 @@ if __name__ == "__main__":
     pygame.display.set_caption('pizzagario')
 
     screen = pygame.display.set_mode(taille_fenetre)
+
+
+    
+
+    play_button_image = pygame.image.load(os.path.join("images","button.png"))
+    play_button = pygame.transform.scale(play_button_image,(400, 180))
+    play_button_rect =play_button.get_rect()
+    play_button_rect.x = math.ceil( screen.get_width()/4)
+    play_button_rect.y =  math.ceil( screen.get_height()/4)
+
+
 
     cam = Camera(1, 0, 0)
     player = Player(screen, cam)
@@ -36,21 +48,42 @@ if __name__ == "__main__":
         for j in range(10):
             bots.append(Food(screen, i*50, j*50, cam))
 
+    is_playing = False
+
+    def ecran_de_demarrage():
+        global is_playing
+        screen.fill((0,255,0))
+        screen.blit(play_button, play_button_rect)
+        
+    RUNNING = True
+
+
 
     # grille  = Grid(screen, taille_fenetre)
-    while True:
+    while RUNNING:
         time.sleep(0.01)
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit(0)
-
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN: 
                 if event.key == pygame.K_SPACE:
-                    player.split(0, 0)
-        pygame.draw.rect(screen, (255,0,0), pygame.Rect(0, 0, 1000, 1000))
-        fonctions_player()
+                    player.split(0, 0)  
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button_rect.collidepoint(event.pos):
+                    is_playing = True
+                    
+                    
+        if not is_playing:
+            ecran_de_demarrage()
+        else:
+            pygame.draw.rect(screen, (255,0,0), pygame.Rect(0, 0, 1000, 1000))
+            fonctions_player()
+            
 
-
+        
         pygame.display.flip()
+
+
+        
