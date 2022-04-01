6d7b8c01247914ea91caeb4b30d5073cb766cdbf
MagnetoCraft <93604302+MagnetoCraft@users.noreply.github.com>
2022-04-01 13:17:20 +0200

    Add files via upload

---
+++ SocketListener.py
@@ -0,0 +1,56 @@
+from ServerManager import ServerManager
+
+class SocketListener():
+    def __init__(self, main):
+        self.main = main
+        self.id = 0
+        self.socket_ids = {}
+        self.clients = []
+        self.serverManager = ServerManager(self)
+
+    def newClient(self, socket):
+        print("new client")
+        self.clients.append(socket)
+        self.socket_ids[socket] = self.id
+        self.id += 1
+        self.main.addPlayer(socket)
+
+    def message(self, socket, event, msg):
+        print(event + " : " + msg)
+        if msg == "stopserver":
+            self.serverManager.closeConnection()
+        else:
+            i = msg.index(",")
+            self.main.setTarget(socket, int(data[:index]), int(data[index+1:]))
+
+    def sendData(data):
+        strdata = ""
+        for socket in data:
+            strdata += self.socket_ids[socket] + ","
+
+            circles = data[socket].circles
+            for circle in circles:
+                strdata += circle.size + ","
+                strdata += circle.x + ","
+                strdata += circle.y + ","
+            strdata += "|"
+        for socket in self.clients:
+            self.serverManager.send(str(self.socket_ids[socket]) + "," + strdata)
+
+    def disconnect(self, socket):
+        self.main.removePlayer(socket)
+        self.clients.remove(socket)
+        del self.socket_ids[socket]
+        print("client disconnect")
+
+    def stop(self):
+        print("the server has stoped")
+
+if __name__ == "__main__":
+
+    socketListener = SocketListener()
+    serverManager = ServerManager(socketListener)
+    continuer = True
+    while continuer:
+        pass
+    serverManager.closeConnection()
